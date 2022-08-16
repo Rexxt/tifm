c9c870db2ad06e6e364a8548dce8eb885d1fcafc
Mizu <40831463+Rexxt@users.noreply.github.com>
2022-08-16 17:26:00 +0200

    global overhaul

--- main.sh
+++ main.sh
@@ -1,124 +1,183 @@
 #!/bin/env bash
-clear
-echo "
-┌─────────────────────────────────────────────────┐
-│ %####                                           │
-│ #########                                       │
-│ @@@###@@# LinFM                                 │
-│ @@@@@@@@@ A file manager, in your command line. │
-│ Made with <3 by [I> (Icycoide)                  │
-└─────────────────────────────────────────────────┘ 
-"
-sleep 3
-echo "Loading contents from " $PWD"..."
-
-clear
-main() {
-    echo "┌─ Waiting for action at" $PWD
-    read -p "└─linfm> " lfm_ans
-    case "$lfm_ans" in
-      SetDir)
-        echo "Select a directory to go to."
-        read lfm_setdir
-        cd $lfm_setdir
-        main
-      ;;
-      Open)
-        echo "Choose a file to open."
-        read lfm_setfile
-        xdg-open $lfm_setfile
-        main
-      ;;
-      Copy)
-        echo "Choose the file and the location you would like to copy it to."
-        read lfm_setfile
-        cp $lfm_setfile
-        main
-      ;;
-      MoveFile)
-        echo "Choose the file and the new location you would like to move it to, with both of them being separated with a space."
-        read lfm_setfile
-        mv $lfm_setfile
-        main
-      ;;
-      Index)
-        ls -a
-        main
-      ;;
-      CreateFolder)
-        echo "Please put in the name of the folder you would like to create."
-        read lfm_setdir
-        mkdir $lfm_setdir
-        main
-      ;;
-      DeleteFile)
-        echo "Select the file you would like to delete. (Without confirmation.) To cancel, just press enter without typing in anything."
-        read lfm_setfile
-        rm $lfm_setfile
-        main
-      ;;
-      DeleteFolder)
-        echo "Select the folder you would like to delete. (Without confirmation.) To cancel, just press enter without typing in anything."
-        read lfm_setdir
-        rm -rf $lfm_setdir
-        main
-      ;;
-      TouchFile)
-        echo "Choose the name of the file..."
-        read lfm_setfile
-        touch $lfm_setfile
-        main
-      ;;
-      PermSet)
-        echo "Choose a file or folder to change the permission"
-        read lfm_setany
-        echo "Choose the arguments from 'chmod' to set as permission for the file or folder. (Read the manual for chmod for more info)"
-        read lfm_setperm
-        chmod $lfm_setperm $lfm_setany
-        main
-      ;;
-      CmdLine)
-        /bin/sh
-        main
-      ;;
-      Help)
-        echo "
-        List of commands:
-        SetDir
-        -Goes to a folder
-        Open
-        -Opens a file
-        MoveFile 
-        -Moves/Renames a file
-        Copy
-        -Copies a file to a location
-        Index
-        -Shows the list of files inside the directory
-        CreateFolder
-        -Creates a folder
-        DeleteFile
-        -Deletes a File
-        DeleteFolder
-        -Deletes folder
-        TouchFile
-        -Creates new file
-        PermSet
-        -Sets permissions for a specific file or folder
-        CmdLine
-        -Switches to command line mode, run 'exit' to exit.
-        Exit
-        -That exits out of LinFM."
-        main
-      ;;
-      Exit)
-        clear
-        exit
-      ;;
-      *)
-        echo "Unrecognized command. Type 'Help' for a list of commands."
-        main
-      ;;
-    esac
+consts() {
+	__TIFM_VERSION="0.1.0"
 }
 
-main
+main() {
+	echo "╭ $PWD"
+	while read file; do
+		echo "│ $file"
+	done < <(ls -1)
+	read -n 1 -p "╰ tifm> " ans
+	if [ "$ans" != "n" ] && [ "$ans" != "r" ]; then
+		echo ""
+	fi
+	case "$ans" in
+		N)
+			echo "Select a directory to go to (/cancel)."
+			read -p "nav:: " tifm_dir
+			if [ "$tifm_dir" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			else
+				cd $tifm_dir
+			fi
+		;;
+		o)
+			echo "Choose a file to open (/cancel)."
+			read -p "from:: " tifm_file
+			if [ "$tifm_file" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			else
+				xdg-open $tifm_file
+			fi
+		;;
+		c)
+			echo "Choose the file and the location you would like to copy it to (/cancel)."
+			read -p "from:: " tifm_file_from
+			if [ "$tifm_file_from" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			fi
+			read -p "to:: " tifm_file_to
+			if [ "$tifm_file_to" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			fi
+			cp "$tifm_file_from" "$tifm_file_to"
+		;;
+		m)
+			echo "Choose the file and the new location you would like to move it to."
+			read -p "from:: " tifm_file_from
+			if [ "$tifm_file_from" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			fi
+			read -p "to:: " tifm_file_to
+			if [ "$tifm_file_to" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			fi
+			mv "$tifm_file_from" "$tifm_file_to"
+		;;
+		i)
+			ls -l -1
+		;;
+		n)
+			read -n 1 tifm_type
+			echo ""
+			case "$tifm_type" in
+				d)
+					echo "Choose the directory you would like to create (/cancel)."
+					read -p "name:: " tifm_dir_name
+					if [ "$tifm_dir_name" == "/cancel" ]; then
+						echo "Cancelled."
+						return
+					fi
+					mkdir "$tifm_dir_name"
+				;;
+				f)
+					echo "Choose the file you would like to create (/cancel)."
+					read -p "name:: " tifm_file_name
+					if [ "$tifm_file_name" == "/cancel" ]; then
+						echo "Cancelled."
+						return
+					fi
+					touch "$tifm_file_name"
+				;;
+				*)
+					echo "Invalid type ([d]irectory/[f]ile)."
+				;;
+			esac
+		;;
+		r)
+			read -n 1 tifm_type
+			echo ""
+			case "$tifm_type" in
+				d)
+					echo "Choose the directory you would like to remove (/cancel)."
+					read -p "name:: " tifm_dir_name
+					if [ "$tifm_dir_name" == "/cancel" ]; then
+						echo "Cancelled."
+						return
+					fi
+					rm -rf "$tifm_dir_name"
+				;;
+				f)
+					echo "Choose the file you would like to remove (/cancel)."
+					read -p "name:: " tifm_file_name
+					if [ "$tifm_file_name" == "/cancel" ]; then
+						echo "Cancelled."
+						return
+					fi
+					rm "$tifm_file_name"
+				;;
+				*)
+					echo "Invalid type ([d]irectory/[f]ile)."
+				;;
+			esac
+		;;
+		p)
+			echo "Choose a file or folder to change the permission (/cancel)."
+			read -p "item:: " tifm_select
+			if [ "$tifm_select" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			fi
+			echo "Choose the arguments from 'chmod' to set as permission for the file or folder (/cancel). (Read the manual for chmod for more info)"
+			read -p "perm:: " tifm_perm
+			if [ "$tifm_perm" == "/cancel" ]; then
+				echo "Cancelled."
+				return
+			fi
+			chmod $tifm_perm $tifm_select
+		;;
+		t)
+			/bin/bash
+		;;
+		"?")
+			echo "
+			List of commands:
+			N
+			- Goes to a folder
+			o
+			- Opens a file
+			m 
+			- Moves/Renames a file
+			c
+			- Copies a file to a location
+			i
+			- Shows the list of files inside the directory (with detail)
+			n
+			- Creates a folder or file
+			r
+			- Removes a folder or file
+			p
+			- Sets permissions for a specific file or folder
+			t
+			- Switches to command line mode, run 'exit' to exit.
+			Q
+			- Quits the program"
+		;;
+		Q)
+			clear
+			exit
+		;;
+		*)
+			echo "Unrecognized command. Type '?' for a list of commands."
+		;;
+	esac
+}
+
+(
+	consts
+	echo "╭ tifm $__TIFM_VERSION
+│ github.com/Rexxt/tifm
+╰ Strike '?' for help
+"
+	echo "Loading contents from $PWD..."
+	while true; do
+		main
+	done
+)
