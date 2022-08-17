95b7d203977ab7c2af1f1986133cd619bb32a63b
Mizu <40831463+Rexxt@users.noreply.github.com>
2022-08-17 15:40:43 +0200

    after quite a bit of fixing, extensions work!

--- extensions/hello.sh
+++ extensions/hello.sh
@@ -4,5 +4,8 @@ greet_user() {
 greet_world() {
     echo "Hello, world!"
 }
-tifmx.bind u greet_user
-tifmx.bind w greet_world
+# tifmx.bind u greet_user
+# tifmx.bind w greet_world
+tifmx.add_long g
+tifmx.bind_sub g u greet_user
+tifmx.bind_sub g w greet_world
--- main.sh
+++ main.sh
@@ -1,48 +1,59 @@
 #!/bin/env bash
+declare -A -x tifm_extensions_commands
+declare -a -x tifm_extensions_longcommands=()
+declare -A -x tifm_extensions_subcommands
+tifmx.bind() {
+	tifm_extensions_commands["$1"]="$2"
+}
+tifmx.add_long() {
+	tifm_extensions_longcommands+=("$1")
+}
+tifmx.bind_sub() {
+	tifm_extensions_subcommands["$1$2"]="$3"
+}
+# consts
+__TIFM_VERSION="0.1.1"
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
 init() {
-	# consts
-	__TIFM_VERSION="0.1.1"
-	BLACK=$(tput setaf 0)
-	RED=$(tput setaf 1)
-	GREEN=$(tput setaf 2)
-	YELLOW=$(tput setaf 3)
-	LIME_YELLOW=$(tput setaf 190)
-	POWDER_BLUE=$(tput setaf 153)
-	BLUE=$(tput setaf 4)
-	MAGENTA=$(tput setaf 5)
-	CYAN=$(tput setaf 6)
-	WHITE=$(tput setaf 7)
-	BRIGHT=$(tput bold)
-	NORMAL=$(tput sgr0)
-	BLINK=$(tput blink)
-	REVERSE=$(tput smso)
-	UNDERLINE=$(tput smul)
-	SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 	# vars
 	STATUS=0
 	# config
 	source "$SCRIPT_DIR/config.sh"
 	# load extensions
-	declare -A tifm_extensions_commands
-	tifm_extensions_longcommands=()
-	declare -A tifm_extensions_subcommands
-	tifmx.bind() {
-		tifm_extensions_commands["$1"]="$2"
-	}
-	tifmx.add_long() {
-		tifm_extensions_longcommands+=("$1")
-	}
-	tifmx.bind_sub() {
-		tifm_extensions_subcommands["$1$2"]="$3"
-	}
+	tput sc
+	printf "Loading extensions... "
 	load_extensions
+	printf "Done.\n"
 }
 
 load_extensions() {
+	local count=0
+	# check how many files are in the extensions directory
+	local files=$(ls -1 "$SCRIPT_DIR/extensions" | wc -l)
 	for extension in "$SCRIPT_DIR/extensions/"*; do
 		source "$extension"
+		count=$((count + 1))
+		# percentage of completion
+		local percent=$((count / files * 100))
+		tput rc
+		printf "Loading extensions... $count/$files ($percent%%)\n"
 	done
-	echo "${tifm_extensions_commands[@]}, ${tifm_extensions_longcommands[@]}, ${tifm_extensions_subcommands[@]}"
 }
 
 main() {
@@ -51,7 +62,7 @@ main() {
 		echo "$__TIFM_DECO_COLOUR$__VBAR $__TIFM_LS_COLOUR$file$NORMAL"
 	done < <(ls -a)
 	read -n 1 -p "$__TIFM_DECO_COLOUR$__ANGLE_DOWN_RIGHT $(__TIFM_PROMPT) $YELLOW" ans
-	if [ "$ans" != "n" ] && [ "$ans" != "r" ] && [ -z "${tifm_extensions_longcommands[$tifm_select]}" ]; then
+	if [[ "$ans" != "n" ]] && [[ "$ans" != "r" ]] && [[ ! "${tifm_extensions_longcommands[*]}" =~ "$ans" ]]; then
 		echo ""
 	fi
 	printf "$NORMAL"
@@ -59,7 +70,7 @@ main() {
 		N)
 			echo "Select a directory to go to (/cancel)."
 			read -p "nav:: " tifm_dir
-			if [ "$tifm_dir" == "/cancel" ]; then
+			if [[ "$tifm_dir" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			else
@@ -69,7 +80,7 @@ main() {
 		o)
 			echo "Choose a file to open (/cancel)."
 			read -p "file:: " tifm_file
-			if [ "$tifm_file" == "/cancel" ]; then
+			if [[ "$tifm_file" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			else
@@ -79,7 +90,7 @@ main() {
 		p)
 			echo "Choose a file to view (/cancel)."
 			read -p "file:: " tifm_file
-			if [ "$tifm_file" == "/cancel" ]; then
+			if [[ "$tifm_file" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			else
@@ -89,7 +100,7 @@ main() {
 		e)
 			echo "Choose a file to edit (/cancel)."
 			read -p "file:: " tifm_file
-			if [ "$tifm_file" == "/cancel" ]; then
+			if [[ "$tifm_file" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			else
@@ -99,12 +110,12 @@ main() {
 		c)
 			echo "Choose the file and the location you would like to copy it to (/cancel)."
 			read -p "from:: " tifm_file_from
-			if [ "$tifm_file_from" == "/cancel" ]; then
+			if [[ "$tifm_file_from" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			fi
 			read -p "to:: " tifm_file_to
-			if [ "$tifm_file_to" == "/cancel" ]; then
+			if [[ "$tifm_file_to" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			fi
@@ -113,12 +124,12 @@ main() {
 		m)
 			echo "Choose the file and the new location you would like to move it to."
 			read -p "from:: " tifm_file_from
-			if [ "$tifm_file_from" == "/cancel" ]; then
+			if [[ "$tifm_file_from" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			fi
 			read -p "to:: " tifm_file_to
-			if [ "$tifm_file_to" == "/cancel" ]; then
+			if [[ "$tifm_file_to" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			fi
@@ -127,7 +138,7 @@ main() {
 		i)
 			echo "Choose the directory to inspect (/cancel)."
 			read -p "dir:: " tifm_dir
-			if [ "$tifm_dir" == "/cancel" ]; then
+			if [[ "$tifm_dir" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			else
@@ -141,7 +152,7 @@ main() {
 				d)
 					echo "Choose the directory you would like to create (/cancel)."
 					read -p "name:: " tifm_dir_name
-					if [ "$tifm_dir_name" == "/cancel" ]; then
+					if [[ "$tifm_dir_name" == "/cancel" ]]; then
 						echo "Cancelled."
 						return
 					fi
@@ -150,7 +161,7 @@ main() {
 				f)
 					echo "Choose the file you would like to create (/cancel)."
 					read -p "name:: " tifm_file_name
-					if [ "$tifm_file_name" == "/cancel" ]; then
+					if [[ "$tifm_file_name" == "/cancel" ]]; then
 						echo "Cancelled."
 						return
 					fi
@@ -168,7 +179,7 @@ main() {
 				d)
 					echo "Choose the directory you would like to remove (/cancel)."
 					read -p "name:: " tifm_dir_name
-					if [ "$tifm_dir_name" == "/cancel" ]; then
+					if [[ "$tifm_dir_name" == "/cancel" ]]; then
 						echo "Cancelled."
 						return
 					fi
@@ -177,7 +188,7 @@ main() {
 				f)
 					echo "Choose the file you would like to remove (/cancel)."
 					read -p "name:: " tifm_file_name
-					if [ "$tifm_file_name" == "/cancel" ]; then
+					if [[ "$tifm_file_name" == "/cancel" ]]; then
 						echo "Cancelled."
 						return
 					fi
@@ -191,13 +202,13 @@ main() {
 		P)
 			echo "Choose a file or folder to change the permission (/cancel)."
 			read -p "item:: " tifm_select
-			if [ "$tifm_select" == "/cancel" ]; then
+			if [[ "$tifm_select" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			fi
 			echo "Choose the arguments from 'chmod' to set as permission for the file or folder (/cancel). (Read the manual for chmod for more info)"
 			read -p "perm:: " tifm_perm
-			if [ "$tifm_perm" == "/cancel" ]; then
+			if [[ "$tifm_perm" == "/cancel" ]]; then
 				echo "Cancelled."
 				return
 			fi
@@ -226,13 +237,13 @@ Q      - Quits the program"
 			exit
 		;;
 		*)
-			if [ ! -z "${tifm_extensions_commands[$tifm_select]}" ]; then
-				eval "${tifm_extensions_commands[$tifm_select]}"
-			elif [ ! -z "${tifm_extensions_longcommands[$tifm_select]}" ]; then
+			if [[ ! -z "${tifm_extensions_commands[$ans]}" ]]; then
+				eval "${tifm_extensions_commands[$ans]}"
+			elif [[ "${tifm_extensions_longcommands[*]}" =~ "$ans" ]]; then
 				read -n 1 tifm_subcommand
 				echo ""
-				if [ ! -z "${tifm_extensions_subcommands[$tifm_select$tifm_subcommand]}" ]; then
-					eval "${tifm_extensions_subcommands[$tifm_select$tifm_subcommand]}"
+				if [[ ! -z "${tifm_extensions_subcommands[$ans$tifm_subcommand]}" ]]; then
+					eval "${tifm_extensions_subcommands[$ans$tifm_subcommand]}"
 				else
 					echo "Invalid subcommand."
 				fi
