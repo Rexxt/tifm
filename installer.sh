0eb9202e8bb9825831a39c3a60485d21eaab9131
Mizu <40831463+Rexxt@users.noreply.github.com>
2022-08-18 20:21:11 +0200

    fix installer 1

--- installer.sh
+++ installer.sh
@@ -8,16 +8,15 @@ if ! type git &> /dev/null; then
     exit 1
 fi
 # make sure we're in sudo
-if [ "$EUID" -ne 0 ]; then
-    echo "please run as root"
-    exit 1
-fi
+# if [ "$EUID" -ne 0 ]; then
+#     echo "please run as root"
+#     exit 1
+# fi
 
-# clone repository
 git clone "https://github.com/Rexxt/tifm.git"
-# remove installer.sh
-rm installer.sh
-mv tifm ~/tifm
+rm tifm/installer.sh
+mkdir -p ~/tifm
+mv tifm ~
 echo "where should we install tifm?"
 echo "1) /usr/local/bin"
 echo "2) /usr/bin"
@@ -26,12 +25,14 @@ echo
 case "$reply" in
     1)
         echo "installing to /usr/local/bin"
-        echo "chmod +x ~/tifm/main.sh; ~/tifm/main.sh" /usr/local/bin/tifm
+        touch /usr/local/bin/tifm
+        echo "chmod +x ~/tifm/main.sh; ~/tifm/main.sh" > /usr/local/bin/tifm
         chmod +x /usr/local/bin/tifm
     ;;
     2)
         echo "installing to /usr/bin"
-        echo "chmod +x ~/tifm/main.sh; ~/tifm/main.sh" /usr/bin/tifm
+        touch /usr/bin/tifm
+        echo "chmod +x ~/tifm/main.sh; ~/tifm/main.sh" > /usr/bin/tifm
         chmod +x /usr/bin/tifm
     ;;
     *)
@@ -39,3 +40,10 @@ case "$reply" in
         exit 1
     ;;
 esac
+# check if tifm command is in path
+if ! type tifm &> /dev/null; then
+    echo "tifm is not in path... this is most likely a problem on our side."
+    exit 1
+else
+    echo "tifm is installed and usable!"
+fi
